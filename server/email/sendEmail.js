bcc8413bfc48dd90b5929bfdaf3f29a8b62dbc35
josephJJ1020 <the.josephfernando@gmail.com>
2022-09-27 15:25:26 -0500

    Commented out sendEmail.js

--- server/email/sendEmail.js
+++ server/email/sendEmail.js
@@ -1,30 +1,30 @@
-require("dotenv").config();
-var nodemailer = require("nodemailer");
+// require("dotenv").config();
+// var nodemailer = require("nodemailer");
 
-var transporter = nodemailer.createTransport({
-  //   service: 'yahoo',
-  //   host: 'smtp.mail.yahoo.com',
-  //   port: 465,
-  //   secure: false,
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: "the.josephfernando@gmail.com",
-    pass: "bhqasrtokuctuzfi",
-  },
-});
+// var transporter = nodemailer.createTransport({
+//   //   service: 'yahoo',
+//   //   host: 'smtp.mail.yahoo.com',
+//   //   port: 465,
+//   //   secure: false,
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   auth: {
+//     user: "the.josephfernando@gmail.com",
+//     pass: "bhqasrtokuctuzfi",
+//   },
+// });
 
-var mailOptions = {
-  from: "the.josephfernando@gmail.com",
-  to: process.env.YAHOO_EMAIL,
-  subject: "Sending Email using Node.js",
-  text: "That was easy!",
-};
+// var mailOptions = {
+//   from: "the.josephfernando@gmail.com",
+//   to: process.env.YAHOO_EMAIL,
+//   subject: "Sending Email using Node.js",
+//   text: "That was easy!",
+// };
 
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Email sent: " + info.response);
-  }
-});
+// transporter.sendMail(mailOptions, function (error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent: " + info.response);
+//   }
+// });
