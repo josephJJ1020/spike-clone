3e513df31aaa3fbab93b264f21457ed2334ba7f9
josephJJ1020 <the.josephfernando@gmail.com>
2022-10-06 15:03:07 -0500

    Ready server for deployment

--- client/src/store/slices/conversationsSlice.js
+++ client/src/store/slices/conversationsSlice.js
@@ -10,18 +10,14 @@ const conversationsSlice = createSlice({
       state.conversations = action.payload;
     },
     replaceConvo: (state, action) => {
-      console.log(action.payload._id);
       if (
         !state.conversations.find((convo) => convo._id === action.payload._id)
       ) {
-        console.log("can't find convo");
         state.conversations.push(action.payload);
       } else {
-        console.log("updating convo");
         state.conversations = [
           ...state.conversations.map((convo) => {
             if (convo._id === action.payload._id) {
-              console.log("returning updated convo");
               return action.payload;
             } else {
               return convo;
